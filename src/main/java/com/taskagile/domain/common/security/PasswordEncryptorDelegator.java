9ac2d9b872f08768dbc90cd8eaf4b2ab61500230
one_1ight <one_1ight@hotmail.com>
2019-10-01 23:31:07 +0800

    fix:invalid return value of encrypt()

--- src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -7,6 +7,6 @@ public class PasswordEncryptorDelegator implements PasswordEncryptor {
   @Override
   public String encrypt(String rawPassword) {
     //TODO implement this
-    return null;
+    return rawPassword;
   }
 }
